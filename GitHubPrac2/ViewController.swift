ac18ef8e6142ade85d01acef5bb49b9115841add
furqan <famanji5010@stu.d214.org>
2022-04-06 14:02:12 -0500

    Initial Commit

---
+++ GitHubPrac2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubPrac2
+//
+//  Created by furqan amanji on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
