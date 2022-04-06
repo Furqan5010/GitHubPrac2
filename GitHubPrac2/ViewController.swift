dd8863e6e81020063a1602e211a40b876c0d492e
knguyen5259 <91755924+knguyen5259@users.noreply.github.com>
2022-04-06 14:15:15 -0500

    Added delegate methods

--- GitHubPrac2/ViewController.swift
+++ GitHubPrac2/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    let animals: [String] = [
+        "Dog",
+        "Cat",
+        "Bird"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.text = animals[indexPath.row]// as! String)
+        return cell
+    }
+    
 }
 
